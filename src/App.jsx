0bbd7f58e6d619657a49f4846789c5ba69771ec5
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-11-21 18:50:18 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Crypto from './components/crypto/crypto';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Crypto/>
+    </div>
+  );
+}
+
+export default App;
